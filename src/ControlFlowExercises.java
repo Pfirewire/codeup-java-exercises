c251e8dbb11f37df5ae037c2bac044a63bfb802f
Stephen Pflug <stephen@pflug.us>
2022-08-24 16:15:37 -0500

    letter grades from numerical exercise and bonus complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -6,11 +6,34 @@ public class ControlFlowExercises {
         scanner.useDelimiter("\n");
         String userResponse;
         do {
-            System.out.print("What number would you like the table of powers to go to? ");
-            int userInt = scanner.nextInt();
-            System.out.printf("Here is your table!%n%nnumber    |squared   |cubed     %n----------|----------|----------%n");
-            for (int i = 1; i <= userInt; i++) {
-                System.out.printf("%-10d|%-10d|%-10d%n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+            System.out.print("Enter a numerical grade from 0 to 100: ");
+            int grade = scanner.nextInt();
+            if (grade >= 98) {
+                System.out.printf("%s percent is an A+%n", grade);
+            } else if (grade >= 92) {
+                System.out.printf("%s percent is an A%n", grade);
+            } else if (grade >= 90) {
+                System.out.printf("%s percent is an A-%n", grade);
+            } else if (grade >= 88) {
+                System.out.printf("%s percent is a B+%n", grade);
+            } else if (grade >= 82) {
+                System.out.printf("%s percent is a B%n", grade);
+            } else if (grade >= 80) {
+                System.out.printf("%s percent is a B-%n", grade);
+            } else if (grade >= 78) {
+                System.out.printf("%s percent is a C+%n", grade);
+            } else if (grade >= 72) {
+                System.out.printf("%s percent is a C%n", grade);
+            } else if (grade >= 70) {
+                System.out.printf("%s percent is a C-%n", grade);
+            } else if (grade >= 68) {
+                System.out.printf("%s percent is a D+%n", grade);
+            } else if (grade >= 62) {
+                System.out.printf("%s percent is a D%n", grade);
+            } else if (grade >= 60) {
+                System.out.printf("%s percent is a D-%n", grade);
+            } else {
+                System.out.printf("%s percent is an F%n", grade);
             }
             System.out.print("Do you wish to try again? Y/N ");
             scanner.nextLine();
