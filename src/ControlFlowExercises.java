80eaab330ea021341007045af64c204b4bc966fc
Stephen Pflug <stephen@pflug.us>
2022-08-24 15:54:38 -0500

    table of powers exercise added user ability to continue or not

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,12 +4,17 @@ public class ControlFlowExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         scanner.useDelimiter("\n");
-
-        System.out.print("Enter an integer: ");
-        int userInt = scanner.nextInt();
-        System.out.printf("Here is your table!%n%nnumber    |squared   |cubed     %n----------|----------|----------%n");
-        for(int i = 1; i <= userInt; i++) {
-            System.out.printf("%-10d|%-10d|%-10d%n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
-        }
+        String userResponse;
+        do {
+            System.out.print("What number would you like the table of powers to go to? ");
+            int userInt = scanner.nextInt();
+            System.out.printf("Here is your table!%n%nnumber    |squared   |cubed     %n----------|----------|----------%n");
+            for (int i = 1; i <= userInt; i++) {
+                System.out.printf("%-10d|%-10d|%-10d%n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+            }
+            System.out.print("Do you wish to try again? Y/N ");
+            scanner.nextLine();
+            userResponse = scanner.nextLine();
+        } while (userResponse.equals("y") || userResponse.equals("Y"));
     }
 }
