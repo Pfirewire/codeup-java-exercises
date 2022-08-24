f31fc99a5a95030f0c60821ac6e87f458d6a0164
Stephen Pflug <stephen@pflug.us>
2022-08-24 10:49:07 -0500

    syntax, types, and variables exercises 5 and 6 complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,8 +5,12 @@ public class HelloWorld {
 
         int myFavoriteNumber = 12;
         System.out.println(myFavoriteNumber);
-        String myString = 3.14159;
+        String myString = "some string";
+        // String myString = 'some string';
+        // String myString = 3.14159;
         System.out.println(myString);
+        long myNumber = 3.14;
+        System.out.println(myNumber);
     }
 }
 
