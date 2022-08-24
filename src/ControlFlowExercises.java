aad0518affd1c12ce90041626efdc37b61ccad28
Stephen Pflug <stephen@pflug.us>
2022-08-24 15:42:44 -0500

    table of powers exercise complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,16 +2,14 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 1; i <= 100; i++){
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(i % 3 == 0){
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
-            }
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+
+        System.out.print("Enter an integer: ");
+        int userInt = scanner.nextInt();
+        System.out.printf("Here is your table!%n%nnumber    |squared   |cubed     %n----------|----------|----------%n");
+        for(int i = 1; i <= userInt; i++) {
+            System.out.printf("%-10d|%-10d|%-10d%n", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
         }
     }
 }
