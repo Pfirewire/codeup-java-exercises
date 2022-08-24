cb876b134a133e0925400fb4466f9e1c585c33cf
Stephen Pflug <stephen@pflug.us>
2022-08-24 14:56:26 -0500

    do while basic exercise: 2 squared until 65536 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 100;
+        int i = 2;
+        double square;
         do {
             System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+            square = Math.pow(i, 2);
+            i = (int) square;
+        } while (i < 1000000);
     }
 }
