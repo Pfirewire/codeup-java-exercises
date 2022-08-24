2a16fa979c554212ec71552ec08f6dedcbf63d4f
Stephen Pflug <stephen@pflug.us>
2022-08-24 15:05:25 -0500

    while exercise refactored with for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 2;
-        double square;
-        do {
-            System.out.println(i);
-            square = Math.pow(i, 2);
-            i = (int) square;
-        } while (i < 1000000);
+        for(int i = 5; i <= 15; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
