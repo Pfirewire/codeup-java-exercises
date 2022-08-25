b8711e640e4b826a9b6e289243ebd68d12c5149b
Stephen Pflug <stephen@pflug.us>
2022-08-25 10:27:14 -0500

    methods first bonus complete

---
+++ src/MethodsExercises.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+
+        System.out.print("Enter a number: ");
+        int userNum1 = scanner.nextInt();
+        System.out.print("Enter another number: ");
+        int userNum2 = scanner.nextInt();
+
+        System.out.printf("%s + %s is: %s%n", userNum1, userNum2, addition(userNum1, userNum2));
+        System.out.printf("%s - %s is: %s%n", userNum1, userNum2, subtraction(userNum1, userNum2));
+        System.out.printf("%s * %s is: %s%n", userNum1, userNum2, multiplication(userNum1, userNum2));
+        System.out.printf("%s / %s is: %s%n", userNum1, userNum2, division(userNum1, userNum2));
+        System.out.printf("%s %% %s is: %s%n", userNum1, userNum2, modulus(userNum1, userNum2));
+    }
+
+    public  static int addition(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiplication(int num1, int num2) {
+        int total = 0;
+        for(int i = 0; i < num2; i++){
+            total += num1;
+        }
+        return total;
+    }
+
+    public static float division(int num1, int num2) {
+        return (float) num1 / (float) num2;
+    }
+
+    public static int modulus(int num1, int num2) {
+        return num1 % num2;
+    }
+}
