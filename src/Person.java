ae7c0586f03ce9871badbfca9c442de1a5ef63bf
Stephen Pflug <stephen@pflug.us>
2022-08-25 12:48:32 -0500

    java objects understanding references exercise complete

--- src/Person.java
+++ src/Person.java
@@ -28,5 +28,40 @@ public class Person {
         p1.sayHello();
         Person p2 = new Person();
         p2.sayHello();
+
+        //I think this will output:
+        //true
+        //error
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+        //It seems I was wrong and person1 == person2 is in fact false
+
+        //I think this will output:
+        //false
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+        //I was wrong, I think it's because it's not comparing the Person.name with the '==' but instead the actual Person objects themselves, which can be ==
+
+        //I think this will output:
+        //John
+        //John
+        //John
+        //Jane
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        //I don't really know why running person2.setName("Jane"); would change the output of person1.getName() from John to Jane
+        //hmmm
+
+
+
+
     }
 }
