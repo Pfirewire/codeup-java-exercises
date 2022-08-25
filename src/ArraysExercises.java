8e135a5afd70ce0c0f4282a1dc569b05306acae5
Stephen Pflug <stephen@pflug.us>
2022-08-25 14:24:04 -0500

    java array basics exercise 1 complete

---
+++ src/ArraysExercises.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class ArraysExercises {
+
+    public static Person[] addPerson(Person[] people, Person person) {
+        Person[] morePeople = new Person[people.length+1];
+        for(int i = 0; i < people.length; i++) {
+            morePeople[i] = people[i];
+        }
+        morePeople[morePeople.length - 1] = person;
+        return morePeople;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(numbers));
+
+        Person[] people = new Person[3];
+        people[0] = new Person("Rich");
+        people[1] = new Person("Steve");
+        people[2] = new Person("Valeria");
+        for(Person person : people) {
+            System.out.println(person.getName());
+        }
+
+        Person[] morePeople = addPerson(people, new Person("Jazmine"));
+        for(Person person : morePeople) {
+            System.out.println(person.getName());
+        }
+
+    }
+
+}
