230fe9910cdf504f133ab3552f4863d32cbc31d9
Stephen Pflug <stephen@pflug.us>
2022-08-26 09:33:19 -0500

    changes tested successfully

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,7 +4,7 @@ public class ShapesTest {
     public static void main(String[] arg) {
         Quadrilateral box1 = new Rectangle(5, 4);
         System.out.printf("Perimeter: %s%nArea: %s%n", box1.getPerimeter(), box1.getArea());
-//        Rectangle box2 = new Square(5);
-//        System.out.printf("Perimeter: %s%nArea: %s%n", box2.getPerimeter(), box2.getArea());
+        Quadrilateral box2 = new Square(5);
+        System.out.printf("Perimeter: %s%nArea: %s%n", box2.getPerimeter(), box2.getArea());
     }
 }
