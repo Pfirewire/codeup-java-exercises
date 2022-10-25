383b1b685945a47c9a377b8b4d94d946504fb92f
Stephen Pflug <stephen@pflug.us>
2022-10-25 14:30:56 -0500

    code added for checking quiz answers

---
+++ src/quizzes/ArithmeticSample.java
@@ -0,0 +1,10 @@
+package quizzes;
+
+public class ArithmeticSample {
+    public static void main(String[] args) {
+        int x = 5 * 4 % 3;
+        System.out.println(x);
+        String y = "hello";
+        System.out.println(y + 5);
+    }
+}
